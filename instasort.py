b9d2b27c95da074b81401c591588d0f09f3b908e
Chirag Gupta <echirag@gmail.com>
2014-03-01 13:54:08 +0530

    Cleaning up

--- instasort.py
+++ instasort.py
@@ -3,20 +3,21 @@ import json
 import time
 import exiftool
 
-def traverse(rootdir, filters):
+def fetchFilesWithExtensions(rootdir, extensions):
     matchedfiles = []
     dirs = [rootdir]
     while dirs:
         cur_dir = dirs[0]
         del dirs[0]
-        walk_result = os.walk(cur_dir).next()   # (path, dirnames, filenames)
+        # (path, dirnames, filenames)
+        walk_result = os.walk(cur_dir).next()
 
         for subdir in walk_result[1]:
             dirs.append(os.path.join(cur_dir, subdir))
 
         for f in walk_result[2]:
             extension = os.path.splitext(f)[1].lower()
-            if extension.startswith('.') and extension[1:] in filters:
+            if extension.startswith('.') and extension[1:] in extensions:
                 matchedfiles.append(os.path.join(cur_dir, f))
 
     return matchedfiles
@@ -34,24 +35,27 @@ def printMetadata(files):
         metadata = et.get_metadata_batch(files)
     for md in metadata:
         print md['SourceFile']
-        year, month, selectedkey = 9999, 99, ''
+        year, month, selectedkey = 99999, 99, ''
         for k in md.keys():
             klower = k.lower()
             if 'date' not in klower:
                 del md[k]
-            else:
-                if klower == 'exif:datetimeoriginal':
-                    year, month = getYearAndMonth(md[k])
-                    selectedkey = k
-                    break
-                else:
-                    thisyear, thismonth = getYearAndMonth(md[k])
-                    if thisyear <= year and thismonth < month:
-                        year, month, selectedkey = thisyear, thismonth, k
-        print year, month
+                continue
+            thisyear, thismonth = getYearAndMonth(md[k])
+            if klower == 'exif:datetimeoriginal':
+                year, month, selectedkey = thisyear, thismonth, k
+                break
+            if thisyear <= year and thismonth < month:
+                year, month, selectedkey = thisyear, thismonth, k
+
+        # Check if valid year/month were found
+        if year != 9999 and month != 99:
+            print "Year: ", year
+            print "Month: ", month
+            print
 
 if __name__ == '__main__':
-    filestoprocess = traverse('/Users/cgupta/Desktop', ['jpg', 'jpeg', 'png', 'gif', 'mp4', 'avi',])
+    filestoprocess = fetchFilesWithExtensions('/Users/cgupta/Desktop', ['jpg', 'jpeg', 'png', 'gif', 'mp4', 'avi',])
     printMetadata(filestoprocess)
 
     print
