6e8a61257517ff449805e7e92d5cf5304bd2c3d0
Chirag Gupta <echirag@gmail.com>
2014-03-03 23:29:39 +0530

    Restructured into smaller, cleaner methods

--- instasort.py
+++ instasort.py
@@ -3,6 +3,8 @@ import time
 import exiftool
 import json
 
+LOGS_ENABLED = True
+
 def findFiles(rootdir, extensions):
     matchedfiles = []
     for root, dirs, files in os.walk(rootdir):
@@ -11,97 +13,6 @@ def findFiles(rootdir, extensions):
             if extension.startswith('.') and extension[1:] in extensions:
                 matchedfiles.append(os.path.join(root, f))
     return matchedfiles
-
-
-def removeEmptyFolders(rootdir):
-    alldirs = []
-    for root, dirs, files in os.walk(rootdir):
-        for d in dirs:
-            alldirs.append(os.path.join(root, d))
-        for f in files:
-            if f.startswith("."):
-                os.remove(os.path.join(root, f))
-                print f
-
-    for d in reversed(alldirs):
-        if os.listdir(d):
-            continue
-        try:
-            os.rmdir(d)
-            print "Deleted", d
-        except:
-            print "ERROR: Could not delete", d
-            pass
-
-
-def parseYearAndMonth(datestring):
-    year, month = 9999, 99
-    if isinstance(datestring, basestring) and len(datestring) > 10:
-        try:
-            year = int(datestring[0:4])
-            month = int(datestring[5:7])
-        except ValueError:
-            pass
-    return year, month
-
-
-def collectDateMetadata(files, print_logs=False):
-    dateMetadata = {}
-    with exiftool.ExifTool() as et:
-        allmetadata = et.get_metadata_batch(files)
-    for metadata in allmetadata:
-        good_exif_found = False
-        filename = metadata['SourceFile']
-        if print_logs:
-            print "File:", filename
-        year, month, selectedkey = 99999, 99, ''
-
-        for k in metadata.keys():
-            klower = k.lower()
-            if 'date' not in klower or klower.startswith('icc_profile'):
-                continue
-            thisyear, thismonth = parseYearAndMonth(metadata[k])
-            if klower == 'exif:datetimeoriginal':
-                year, month, selectedkey = thisyear, thismonth, k
-                good_exif_found = True
-                break
-            if thisyear < year or (thisyear == year and thismonth < month):
-                year, month, selectedkey = thisyear, thismonth, k
-                if print_logs:
-                    print "Metadata Tag:", selectedkey, metadata[selectedkey]
-
-        if not good_exif_found:
-            selectedtime = modifiedtime = time.gmtime(os.path.getmtime(filename))
-            createdtime = time.gmtime(os.path.getctime(filename))
-            if createdtime < modifiedtime:
-                selectedtime = createdtime
-            selectedmonth = int(time.strftime("%m", selectedtime))
-            selectedyear = int(time.strftime("%Y", selectedtime))
-            if print_logs:
-                print "File Timestamp:", time.strftime("%Y:%m:%d %H:%M:%S", (selectedtime))
-
-            if selectedyear < year or (selectedyear == year and selectedmonth < month):
-                year, month, selectedkey = selectedyear, selectedmonth, None
-
-        # Check if valid year/month were found
-        if year != 9999 and month != 99:
-            if print_logs:
-                if selectedkey:
-                    print "Selected Tag:", selectedkey, metadata[selectedkey]
-                else:
-                    print "Using File Timestamp"
-                print
-            year = str(year)
-            month = str(month).zfill(2)
-            # Construct multi level dict with year: month: [filenames]
-            if not year in dateMetadata:
-                dateMetadata[year] = {}
-            if not month in dateMetadata[year]:
-                dateMetadata[year][month] = []
-            dateMetadata[year][month].append(filename)
-        else:
-            print "ERROR: No valid date found in", filename
-    return dateMetadata
 
 
 def makeFolder(folderpath):
@@ -128,8 +39,104 @@ def organizeFiles(rootdir, metadata):
                     pass
 
 
+def removeEmptyFolders(rootdir):
+    alldirs = []
+    for root, dirs, files in os.walk(rootdir):
+        for d in dirs:
+            alldirs.append(os.path.join(root, d))
+        for f in files:
+            if f.startswith("."):
+                os.remove(os.path.join(root, f))
+                print f
+
+    for d in reversed(alldirs):
+        if os.listdir(d):
+            continue
+        try:
+            os.rmdir(d)
+            print "Deleted", d
+        except:
+            print "ERROR: Could not delete", d
+            pass
+
+
+# Returns (year, month)
+def parseYearAndMonth(datestring):
+    year, month = 99999, 99
+    if isinstance(datestring, basestring) and len(datestring) > 10:
+        try:
+            year = int(datestring[0:4])
+            month = int(datestring[5:7])
+        except ValueError:
+            pass
+    return year, month
+
+
+# Returns (year, month) or False
+def getCreatedDate(filename, exifdata):
+    year, month = 99999, 99
+    for tag in exifdata:
+        taglower = tag.lower()
+        if 'date' not in taglower or taglower.startswith('icc_profile'):
+            continue
+        thisyear, thismonth = parseYearAndMonth(exifdata[tag])
+        if LOGS_ENABLED:
+            print "EXIF:", tag, exifdata[tag]
+
+        if taglower == 'exif:datetimeoriginal':
+            return thisyear, thismonth
+
+        if thisyear < year or (thisyear == year and thismonth < month):
+            year, month = thisyear, thismonth
+
+    # Read file stats since the ideal tag wasn't found
+    filetime = modifiedtime = time.gmtime(os.path.getmtime(filename))
+    createdtime = time.gmtime(os.path.getctime(filename))
+    if createdtime < modifiedtime:
+        filetime = createdtime
+    filemonth = int(time.strftime("%m", filetime))
+    fileyear = int(time.strftime("%Y", filetime))
+    if LOGS_ENABLED:
+        print "File Timestamp:", time.strftime("%Y:%m:%d %H:%M:%S", (filetime))
+
+    if fileyear < year or (fileyear == year and filemonth < month):
+        year, month = fileyear, filemonth
+
+    if year != 99999 and month != 99:
+        return year, month
+
+    return False
+
+
+def collectDateMetadata(files):
+    date_metadata = {}
+    with exiftool.ExifTool() as et:
+        allmetadata = et.get_metadata_batch(files)
+    for metadata in allmetadata:
+        filename = metadata['SourceFile']
+        del metadata['SourceFile']
+        if LOGS_ENABLED:
+            print "File:", filename
+
+        created_date = getCreatedDate(filename, metadata)
+        if created_date:
+            year = str(created_date[0])
+            month = str(created_date[1]).zfill(2)
+            if LOGS_ENABLED:
+                print "Month Created: %s, %s" % (month, year)
+                print
+            # Construct multi level dict with year: month: [filenames]
+            if not year in date_metadata:
+                date_metadata[year] = {}
+            if not month in date_metadata[year]:
+                date_metadata[year][month] = []
+            date_metadata[year][month].append(filename)
+        else:
+            print "ERROR: No valid date found for", filename
+    return date_metadata
+
+
 if __name__ == '__main__':
-    LOGS_ENABLED = False
     rootdir = '/Users/cgupta/Downloads/InstaSort'
     filetypes = ['jpg', 'jpeg', 'png', 'gif', 'mp4', 'avi',]
     filteredfiles = findFiles(rootdir, filetypes)
@@ -138,12 +145,11 @@ if __name__ == '__main__':
         print "ERROR: No media files found."
         exit(0)
 
-    dateMetadata = collectDateMetadata(filteredfiles, print_logs=LOGS_ENABLED)
+    date_metadata = collectDateMetadata(filteredfiles)
     if LOGS_ENABLED:
-        print json.dumps(dateMetadata, indent=1, sort_keys=True)
+        print json.dumps(date_metadata, indent=1, sort_keys=True)
 
-    organizeFiles(rootdir, dateMetadata)
+    organizeFiles(rootdir, date_metadata)
 
     removeEmptyFolders(rootdir)
     print "\nDone :)"
-
